e3cc00c83aae66886848e6c626c584f786694e5f
Halouiti <1597154@uab.cat>
2026-05-17 18:26:13 +0200

    Canvi import, a vegades falla al carregar createDbWorker

--- src/db/dbClient.js
+++ src/db/dbClient.js
@@ -1,5 +1,5 @@
 import sqljsHttpvfs from "sql.js-httpvfs";
-import { createDbWorker } from "sql.js-httpvfs";
+const { createDbWorker } = sqljsHttpvfs.default || sqljsHttpvfs;
 
 const isNode = typeof Worker === "undefined";
 
