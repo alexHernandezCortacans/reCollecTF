6ea695cacda93658352fe6126c51680e7d446fd2
Ivan Erill <ivanerill@users.noreply.github.com>
2026-05-28 12:05:02 +0200

    Update dbClient.js to reflect change in vite base

--- curation-pipeline/src/db/dbClient.js
+++ curation-pipeline/src/db/dbClient.js
@@ -10,7 +10,7 @@ export const dbWorkerPromise = createDbWorker(
       from: "inline",
       config: {
         serverMode: "full",
-        url: "/reCollecTF/CollecTF.db.gz",
+        url: "/CollecTF.db.gz",
         requestChunkSize: 4096,  // fewer, larger requests
       },
     },
