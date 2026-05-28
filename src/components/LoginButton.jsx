7b9296e761b389fc311a0121234821c855f9f30f
Ivan Erill <ivanerill@users.noreply.github.com>
2026-05-28 12:14:04 +0200

    Update LoginButton.jsx to reflect changes in vite base

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -10,12 +10,12 @@ function LoginButton({ userStatus, user, loading }) {
         {loading && <div className="animate-pulse text-gray-500 font-semibold">Loading...</div>}
         {userStatus == 0 && !loading &&
           <a className="text-accent no-underline" href={loginUrl}>
-            <button className="btn bg-green-600 hover:bg-green-900 flex items-center gap-2 text-black"><img src="/reCollecTF/assets/loginout.png" alt="Log in" className="w-6 h-6"></img>Log In</button>
+            <button className="btn bg-green-600 hover:bg-green-900 flex items-center gap-2 text-black"><img src="/assets/loginout.png" alt="Log in" className="w-6 h-6"></img>Log In</button>
           </a>
         }
         {(userStatus == 1 || userStatus == 2) && !loading &&
           <a className="text-accent no-underline" href={logoutUrl}>
-            <button className="btn bg-red-700 hover:bg-red-900 flex items-center gap-2 text-black"><img src="/reCollecTF/assets/loginout.png" alt="Log out" className="w-6 h-6"></img>Log Out</button>
+            <button className="btn bg-red-700 hover:bg-red-900 flex items-center gap-2 text-black"><img src="/assets/loginout.png" alt="Log out" className="w-6 h-6"></img>Log Out</button>
           </a>
         }
         {(userStatus == 1 || userStatus == 2) && user && !loading && <b>{user}</b>}
