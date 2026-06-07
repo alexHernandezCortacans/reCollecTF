c37a8a1340870ea88851fbfc5229ba7cb1f946e5
Halouiti <1597154@uab.cat>
2026-06-07 18:24:06 +0200

    Passed logic to workflow, new github action

--- src/utils/serverless.js
+++ src/utils/serverless.js
@@ -79,23 +79,17 @@ export async function createUniprodAccessionFile(htmlContent, tf_instance_id, un
 export async function dispatchAndCreate(data, htmlContent, tf_instance_id, uniprotAccession, expressionInfo) {
   const expressionId = expressionIdFromTfInstanceId(tf_instance_id);
 
-  console.log(
-    JSON.stringify({
+  const res = await fetch("https://recollectf2.vercel.app/api/functions/dispatch-and-create", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({
       inputs: data.inputs,
       expressionId,
       htmlContent,
       expressionInfo,
       uniprotAccession,
-    }, null, 2)
-  );
-
-  console.log(JSON.stringify(data, null, 2));
-
-  const res = await fetch("https://recollectf2.vercel.app/api/functions/dispatch-and-create", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    credentials: "include",
-    body: JSON.stringify({ inputs: data.inputs, expressionId, htmlContent, expressionInfo, uniprotAccession }),
+    }),
   });
 
   const text = await res.text();
