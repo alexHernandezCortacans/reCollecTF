3ab66f0132047c78934b7ceca29610baa06c1d8b
Halouiti <1597154@uab.cat>
2026-09-05 11:50:55 +0200

    Fixed import

--- src/components/steps/Step7CurationInfo.jsx
+++ src/components/steps/Step7CurationInfo.jsx
@@ -1,5 +1,5 @@
 // src/components/steps/Step7CurationInfo.jsx
-const zlib = require('zlib');
+import zlib from "zlib";
 import { useMemo, useState } from "react";
 import { useCuration } from "../../contexts/CurationContext";
 import { dispatchAndCreate, dispatchWorkflowPipe } from "../../utils/serverless";
