55ae52912c761247a1091343f5fb30ec435aeda0
Halouiti <1597154@uab.cat>
2026-09-05 12:30:06 +0200

    Typo

--- src/components/steps/Step7CurationInfo.jsx
+++ src/components/steps/Step7CurationInfo.jsx
@@ -1,5 +1,5 @@
 // src/components/steps/Step7CurationInfo.jsx
-import gzip from "pako";
+import { gzip } from "pako";
 import { useMemo, useState } from "react";
 import { useCuration } from "../../contexts/CurationContext";
 import { dispatchAndCreate, dispatchWorkflowPipe } from "../../utils/serverless";
